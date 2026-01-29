eecd9958da0dce1a3a87fb2309d4f5993de1cf6a
Gurts-yo6684 <leonf6017@gmail.com>
2026-01-29 14:41:41 +0000

    args

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,3 +1,30 @@
+
+# def tea_order(customer_name, tea_type, **kwargs):
+#     print(customer_name, "ordered a", tea_type, "tea")
+#     for key, value in kwargs.items():
+#         print("   - Add:", key, ":", value)
+
+
+# tea_order("Alice", "chamomile")
+# tea_order("Bon", "black", milk="oat")
+# tea_order("Tony", "black", milk="oat", sweetener="honey")
+
+
+
+def tea_order(customer_name, tea_type, *args, **kwargs):
+    print(customer_name, "ordered a", tea_type, "tea")
+    for args in args:
+        print("   - Add", args)
+    for key, value in kwargs.items():
+        print("   - Add:", key, ":", value)
+
+tea_order("Alice", "chamomile")
+tea_order("Bon", "black", milk="oat")
+tea_order("Tony", "black", milk="oat", sweetener="honey")
+
+
+
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
